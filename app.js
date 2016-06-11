24a7c6ff5c1d571896d37f34059adb5b2ad38544
Szonja <szonja.pallagi@gmail.com>
2016-06-11 02:49:08 +0200

    Use a cache so that json is dled only once

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ angular.module("three-goals-wc", ['tableSort'])
 	var cache = null;
 
 	var getResults = function() {
-		return $http.get('db.json').then(function(res) {
-			return res.data;
-		});
+		if(cache === null) {
+			return $http.get('db.json').then(function(res) {
+				cache = res.data;
+				Object.freeze(cache);
+				return cache;
+			});
+		} else return $q.when(cache);
 	};
 
 	return {
